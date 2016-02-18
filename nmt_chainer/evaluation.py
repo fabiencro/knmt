ec033d643c2a459e86bcc75ec0c401af5d85ed14
Fabien <fabien.cromieres@gmail.com>
2016-02-18 19:54:04 +0900

    implemented faster beam_search

--- nmt_chainer/evaluation.py
+++ nmt_chainer/evaluation.py
@@ -91,13 +91,14 @@ def greedy_batch_translate(encdec, eos_idx, src_data, batch_size = 80, gpu = Non
     else:
         return res
      
-def beam_search_translate(encdec, eos_idx, src_data, beam_width = 20, nb_steps = 50, gpu = None):
+def beam_search_translate(encdec, eos_idx, src_data, beam_width = 20, nb_steps = 50, gpu = None, beam_opt = False):
     nb_ex = len(src_data)
 #     res = []
     for i in range(nb_ex):
         src_batch, src_mask = make_batch_src([src_data[i]], gpu = gpu, volatile = "on")
-        translations = encdec.beam_search(src_batch, src_mask, nb_steps = nb_steps, eos_idx = eos_idx, beam_width = beam_width)
-        
+        translations = encdec.beam_search(src_batch, src_mask, nb_steps = nb_steps, eos_idx = eos_idx, beam_width = beam_width,
+                                          beam_opt = beam_opt)
+#         print "nb_trans", len(translations), [score for _, score in translations]
         bests = []
         translations.sort(key = itemgetter(1), reverse = True)
         bests.append(translations[0])
