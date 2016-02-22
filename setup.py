e7830da9f64290cc3a50821b2c2abd5119d711d2
Fabien <fabien.cromieres@gmail.com>
2016-02-23 02:02:34 +0900

    added setup.pt

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='nmt_chainer',
+      version='1.0',
+      description='Implementation of RNNSearch and other Neural MT models in Chainer',
+      author='Fabien Cromieres',
+      author_email='fabien.cromieres@gmail.com',
+#       url='https://www.python.org/sigs/distutils-sig/',
+      packages=['nmt_chainer'],
+     )
+
