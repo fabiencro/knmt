047c8805fa544f9e20993321204bbd1ee2788801
Fabien <fabien.cromieres@gmail.com>
2016-11-25 14:07:06 +0900

    Added pass_by option to stacked rnn

--- nmt_chainer/rnn_cells.py
+++ nmt_chainer/rnn_cells.py
@@ -139,7 +139,9 @@ class GatedLSTMCell(Chain):
         return 3 # state + cell + gated_output
     
 class StackedCell(ChainList):
-    def __init__(self, in_size, out_size, cell_type = LSTMCell, nb_stacks = 2, dropout = 0.5):
+    def __init__(self, in_size, out_size, cell_type = LSTMCell, nb_stacks = 2, 
+                 dropout = 0.5, pass_by = False, no_dropout_on_input = False,
+                 no_pass_by_on_output = True):
         nb_stacks = nb_stacks or 2
         cell_type = cell_type or LSTMCell
         
@@ -158,7 +160,10 @@ class StackedCell(ChainList):
         assert len(self) == nb_stacks
         
         self.dropout = dropout
-    
+        self.pass_by = pass_by
+        self.no_pass_by_on_output = no_pass_by_on_output
+        self.no_dropout_on_input = no_dropout_on_input
+        
     def get_initial_states(self, mb_size):
         res = []
         for i in xrange(len(self)):
@@ -171,12 +176,17 @@ class StackedCell(ChainList):
         states_cursor = 0
         res = []
         for i in xrange(len(self)):
-            if self.dropout is not None:
+            if self.dropout is not None and not (self.no_dropout_on_input and i == 0):
                 input_below = F.dropout(input_below, ratio = self.dropout, train = (mode == "train"))
             new_states = self[i](prev_states[states_cursor:states_cursor + self.nb_of_states[i]], input_below,
                                  mode = mode)
             states_cursor += self.nb_of_states[i]
-            input_below = new_states[-1]
+            
+            if self.pass_by and not (i == len(self) -1 and self.no_pass_by_on_output):
+                input_below = new_states[-1] + input_below
+            else:
+                input_below = new_states[-1]
+                
             res += list(new_states)
         return res
     
@@ -200,14 +210,25 @@ class StackedCell(ChainList):
 #         return F.concat((prev_state1, prev_state2), axis = 1)
     
 cell_dict = {
-             "lstm": LSTMCell,
-             "dlstm": StackedCell,
+             "lstm": LSTMCell, 
+             "dlstm": StackedCell, # for backward compatibility
+             "stack": StackedCell,
              "slow_gru": GRUCell,
              "gru": FastGRUCell,
              "glstm": GatedLSTMCell
              }
 
-# has_dropout = set(["dlstm"])
+# has_dropout = set(["dlno_dropout_on_input = Falsestm"])
+
+
+cell_description_keywords = {
+    "dropout": float,
+    "nb_stacks": int,
+    "sub_cell_type": lambda k:cell_dict[k],
+    "pass_by": int,
+    "no_dropout_on_input": int,
+    "no_pass_by_on_output": int,
+    }
 
 def create_cell_model_from_string(model_str):
     components = model_str.split(",")
@@ -217,25 +238,24 @@ def create_cell_model_from_string(model_str):
     for comp in components[1:]:
         assert ":" in  comp
         key, val = comp.split(":")
-        if key == "dropout":
-            keywords[key] = float(val)
-        elif key == "nb_stacks":
-            keywords[key] = int(val)
-        elif key == "sub_cell_type":
-            cell_type = cell_dict[val]
-            keywords[key] = cell_type
+        if key in cell_description_keywords:
+            keywords[key] = cell_description_keywords[key](val)
         else:
-            raise ValueError("bad cell parameter: %s"%comp)
+            raise ValueError("bad cell parameter: %s (possible parameters: %s)"%
+                             (comp, " ".join(cell_description_keywords.keys())))
     return create_cell_model(type_str, **keywords)
 
-def create_cell_model(type_str, dropout = None, nb_stacks = None, sub_cell_type = None):
+def create_cell_model(type_str, **cell_parameters):
+    if type_str not in cell_dict:
+        raise ValueError("bad cell type: %s (possible types: %s)"%
+                             (type_str, " ".join(cell_dict.keys())))
     cell_type = cell_dict[type_str]
     if type_str == "dlstm":
         def instantiate(in_size, out_size):
-            return cell_type(in_size, out_size, dropout = dropout, nb_stacks = nb_stacks, cell_type = sub_cell_type)        
+            return cell_type(in_size, out_size, **cell_parameters)        
     else:
-        assert nb_stacks is None
-        assert dropout is None
+        if len(cell_parameters) != 0:
+            raise ValueError("unexpected cell parameters: %r" % cell_parameters)
         def instantiate(in_size, out_size):
             return cell_type(in_size, out_size)
     return instantiate
