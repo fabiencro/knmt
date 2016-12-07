b04606d21a357480b1ecf1c33fe06f130aa83d63
Frederic Bergeron <bergeron@pa.jst.jp>
2016-12-07 16:08:30 +0900

    Added initializer parameters.

--- nmt_chainer/rnn_cells.py
+++ nmt_chainer/rnn_cells.py
@@ -7,12 +7,14 @@ __email__ = "fabien.cromieres@gmail.com"
 __status__ = "Development"
 
 from _collections import defaultdict
+from collections import Counter
 import numpy as np
 import chainer
 from chainer import cuda, Variable
 from chainer import Link, Chain, ChainList
 import chainer.functions as F
 import chainer.links as L
+from chainer import initializers
 import math, random
 
 from utils import ortho_init
@@ -27,10 +29,10 @@ import faster_gru
 
 
 class GRUCell(Chain):
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, init = None, inner_init = None, bias_init = None):
         log.info("Creating GRUCell(%i, %i)"%(in_size, out_size))
         super(GRUCell, self).__init__(
-            gru = L.GRU(out_size, in_size),
+            gru = L.GRU(out_size, in_size, init = init, inner_init = inner_init, bias_init = bias_init),
         )
         self.add_param("initial_state", (1, out_size))
         self.initial_state.data[...] = self.xp.random.randn(out_size)
@@ -52,10 +54,10 @@ class GRUCell(Chain):
         return 1
     
 class FastGRUCell(Chain):
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, init = None, bias_init = None):
         log.info("Creating GRUCell(%i, %i)"%(in_size, out_size))
         super(FastGRUCell, self).__init__(
-            gru = faster_gru.GRU(out_size, in_size),
+            gru = faster_gru.GRU(out_size, in_size, init = init, bias_init = bias_init),
         )
         self.add_param("initial_state", (1, out_size))
         self.initial_state.data[...] = self.xp.random.randn(out_size)
@@ -77,10 +79,12 @@ class FastGRUCell(Chain):
         return 1
 
 class LSTMCell(Chain):
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, lateral_init = None, upward_init = None, bias_init = None, forget_bias_init = None):
         log.info("Creating LSTMCell(%i, %i)"%(in_size, out_size))
         super(LSTMCell, self).__init__(
-            lstm = L.StatelessLSTM(in_size, out_size),
+            lstm = L.StatelessLSTM(in_size, out_size, 
+                lateral_init = lateral_init, upward_init = upward_init, 
+                bias_init = bias_init, forget_bias_init = forget_bias_init)
         )
         self.add_param("initial_state", (1, out_size))
         self.initial_state.data[...] = self.xp.random.randn(out_size)
@@ -104,12 +108,12 @@ class LSTMCell(Chain):
     
     
 class GatedLSTMCell(Chain):
-    def __init__(self, in_size, out_size):
+    def __init__(self, in_size, out_size, lateral_init = None, upward_init = None, bias_init = None, forget_bias_init = None):
         log.info("Creating GatedLSTMCell(%i, %i)"%(in_size, out_size))
         assert in_size == out_size
         
         super(GatedLSTMCell, self).__init__(
-            lstm = L.StatelessLSTM(in_size, out_size),
+            lstm = L.StatelessLSTM(in_size, out_size, lateral_init = lateral_init, upward_init = upward_init, bias_init = bias_init, forget_bias_init = forget_bias_init),
             gate_w = L.Linear(in_size, in_size)
         )
         self.add_param("initial_state", (1, out_size))
@@ -139,7 +143,7 @@ class GatedLSTMCell(Chain):
         return 3 # state + cell + gated_output
     
 class StackedCell(ChainList):
-    def __init__(self, in_size, out_size, cell_type = LSTMCell, nb_stacks = 2, dropout = 0.5):
+    def __init__(self, in_size, out_size, cell_type = LSTMCell, nb_stacks = 2, dropout = 0.5, init = None, inner_init = None, lateral_init = None, upward_init = None, bias_init = None, forget_bias_init = None):
         nb_stacks = nb_stacks or 2
         cell_type = cell_type or LSTMCell
         
@@ -147,12 +151,26 @@ class StackedCell(ChainList):
         super(StackedCell, self).__init__()
         self.nb_of_states = []
         
-        cell0 = cell_type(in_size, out_size)
+        if cell_type in (LSTMCell, GatedLSTMCell):
+            cell0 = cell_type(in_size, out_size, lateral_init = lateral_init, upward_init = upward_init, bias_init = bias_init, forget_bias_init = forget_bias_init)
+        elif cell_type == GRUCell:
+            cell0 = cell_type(in_size, out_size, init = init, inner_init = inner_init, bias_init = bias_init)
+        elif cell_type == FastGRUCell:
+            cell0 = cell_type(in_size, out_size, init = init, bias_init = bias_init)
+        else:
+            raise ValueError("Unsupported cell_type={0}".format(cell_type))
         self.add_link(cell0)
         self.nb_of_states.append(cell0.get_nb_states())
         
         for i in xrange(1, nb_stacks):
-            cell = cell_type(out_size, out_size)
+            if cell_type in (LSTMCell, GatedLSTMCell):
+                cell = cell_type(out_size, out_size, lateral_init = lateral_init, upward_init = upward_init, bias_init = bias_init, forget_bias_init = forget_bias_init)
+            elif cell_type == GRUCell:
+                cell = cell_type(out_size, out_size, init = init, inner_init = inner_init, bias_init = bias_init)
+            elif cell_type == FastGRUCell:
+                cell = cell_type(out_size, out_size, init = init, bias_init = bias_init)
+            else:
+                raise ValueError("Unsupported cell_type={0}".format(cell_type))
             self.add_link(cell)
             self.nb_of_states.append(cell.get_nb_states())
         assert len(self) == nb_stacks
@@ -215,7 +233,7 @@ def create_cell_model_from_string(model_str):
     
     keywords = {}
     for comp in components[1:]:
-        assert ":" in  comp
+        assert ":" in comp
         key, val = comp.split(":")
         if key == "dropout":
             keywords[key] = float(val)
@@ -224,11 +242,77 @@ def create_cell_model_from_string(model_str):
         elif key == "sub_cell_type":
             cell_type = cell_dict[val]
             keywords[key] = cell_type
+        elif key.endswith("init_type"):
+            keywords[key] = val
+        elif key.endswith("init_scale"):
+            keywords[key] = float(val)
+        elif key.endswith("init_fillvalue"):
+            keywords[key] = float(val)
         else:
             raise ValueError("bad cell parameter: %s"%comp)
     return create_cell_model(type_str, **keywords)
 
-def create_cell_model(type_str, dropout = None, nb_stacks = None, sub_cell_type = None):
+def create_initializer(init_type, scale = None, fillvalue = None):
+    if init_type == 'identity':
+        return initializers.Identity() if scale is None else initializers.Identity(scale = scale)
+    if init_type == 'constant':
+        return initializers.Constant(fillvalue)
+    if init_type == 'zero':
+        return initializers.Zero()
+    if init_type == 'one':
+        return initializers.One()
+    if init_type == 'normal':
+        return initializers.Normal() if scale is None else initializers.Normal(scale)
+    if init_type == 'glorotNormal':
+        return initializers.GlorotNormal() if scale is None else initializers.GlorotNormal(scale)
+    if init_type == 'heNormal':
+        return initializers.HeNormal() if scale is None else initializers.HeNormal(scale)
+    if init_type == 'orthogonal':
+        return initializers.Orthogonal(scale) if scale is None else initializers.Orthogonal(scale)
+    if init_type == 'uniform':
+        return initializers.Uniform(scale) if scale is None else initializers.Uniform(scale)
+    if init_type == 'leCunUniform':
+        return initializers.LeCunUniform(scale) if scale is None else initializers.LeCunUniform(scale)
+    if init_type == 'glorotUniform':
+        return initializers.GlorotUniform(scale) if scale is None else initializers.GlorotUniform(scale)
+    if init_type == 'heUniform':
+        return initializers.HeUniform(scale) if scale is None else initializers.HeUniform(scale)
+    raise ValueError("Unknown initializer type: {0}".format(init_type))
+
+def create_initializer_table(keywords):
+    initializers = {}
+    init_params = [k for k in keywords if 'init_' in k]
+    init_kind = Counter(map ((lambda str: str[:str.index('init') + 4]), init_params)) 
+    for prefix in init_kind:
+        init_type = None
+        key = "{0}_type".format(prefix)
+        if key in keywords:
+            init_type = keywords[key]
+
+        init_scale = None
+        key = "{0}_scale".format(prefix)
+        if key in keywords:
+            init_scale = keywords[key]
+
+        init_fillvalue = None
+        key = "{0}_fillvalue".format(prefix)
+        if key in keywords:
+            init_fillvalue = keywords[key]
+
+        initializer = create_initializer(init_type, init_scale, init_fillvalue)
+        initializers[prefix] = initializer
+    return initializers
+
+def create_cell_model(type_str, dropout = None, nb_stacks = None, sub_cell_type = None, **keywords):
+    initializers = create_initializer_table(keywords)
+    
+    init = initializers['init'] if 'init' in initializers else None 
+    inner_init = initializers['inner_init'] if 'inner_init' in initializers else None
+    lateral_init = initializers['lateral_init'] if 'lateral_init' in initializers else None 
+    upward_init = initializers['upward_init'] if 'upward_init' in initializers else None
+    bias_init = initializers['bias_init'] if 'bias_init' in initializers else None
+    forget_bias_init = initializers['forget_bias_init'] if 'forget_bias_init' in initializers else None
+
     cell_type = cell_dict[type_str]
     if type_str == "dlstm":
         def instantiate(in_size, out_size):
@@ -237,7 +321,14 @@ def create_cell_model(type_str, dropout = None, nb_stacks = None, sub_cell_type 
         assert nb_stacks is None
         assert dropout is None
         def instantiate(in_size, out_size):
-            return cell_type(in_size, out_size)
+            if type_str in ("lstm", "glstm"):
+                return cell_type(in_size, out_size, lateral_init = lateral_init, upward_init = upward_init, bias_init = bias_init, forget_bias_init = forget_bias_init)
+            elif type_str == "slow_gru":
+                return cell_type(in_size, out_size, init = init, inner_init = inner_init, bias_init = bias_init)
+            elif type_str == "gru":
+                return cell_type(in_size, out_size, init = init, bias_init = bias_init)
+            else:
+                raise ValueError("Unsupported cell_type={0}".format(cell_type))
     return instantiate
         
         
