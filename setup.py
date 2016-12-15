04a1c6100be41bc3fde57abf3f21984652a33d03
Fabien <fabien.cromieres@gmail.com>
2016-12-16 02:23:58 +0900

    packaging improvement

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+# from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(name='nmt_chainer',
       version='1.0',
       description='Implementation of RNNSearch and other Neural MT models in Chainer',
       author='Fabien Cromieres',
       author_email='fabien.cromieres@gmail.com',
-#       url='https://www.python.org/sigs/distutils-sig/',
+        #packages=find_packages()
       packages=['nmt_chainer'],
+      url="https://github/fabiencro/knmt",
+      scripts=["nmt_chainer/knmt"],
+      install_requires=['chainer', 'bokeh', 'plotly']
      )
 
