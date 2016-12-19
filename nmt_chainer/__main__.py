c620856bb236ea29442a0f0989c6cb78ec3f3cda
Fabien <fabien.cromieres@gmail.com>
2016-12-19 14:25:30 +0900

    Improved launching script knmt and knmt-debug

--- nmt_chainer/__main__.py
+++ nmt_chainer/__main__.py
@@ -5,10 +5,44 @@ import nmt_chainer.train as train
 import nmt_chainer.eval as eval_module
 import nmt_chainer.make_data as make_data
 
+
+
+def run_in_pdb(func, args):
+    import pdb as pdb_module
+    import sys, traceback
+    pdb = pdb_module.Pdb()
+    while True:
+        try:
+            pdb.runcall(func, args)
+            if pdb._user_requested_quit:
+                break
+            print "The program finished and will be restarted"
+        except pdb_module.Restart:
+            print "Restarting with arguments:"
+            print "\t" + " ".join(sys.argv[1:])
+        except SystemExit:
+            # In most cases SystemExit does not warrant a post-mortem session.
+            print "The program exited via sys.exit(). Exit status: ",
+            print sys.exc_info()[1]
+        except SyntaxError:
+            traceback.print_exc()
+            sys.exit(1)
+        except:
+            traceback.print_exc()
+            print "Uncaught exception. Entering post mortem debugging"
+            print "Running 'cont' or 'step' will restart the program"
+            t = sys.exc_info()[2]
+            pdb.interaction(None, t)
+            print "Post mortem debugger finished. The program will be restarted"
+
+
 def main():
     # create the top-level parser
     parser = argparse.ArgumentParser(description = "Kyoto-NMT: an Implementation of the RNNSearch model", 
                                          formatter_class = argparse.ArgumentDefaultsHelpFormatter)
+    
+    parser.add_argument("--run_in_pdb", default = False, action = "store_true", help = "run knmt in pdb (python debugger)")
+    
     subparsers = parser.add_subparsers()
     
     # create the parser for the "make_data" command
@@ -29,7 +63,13 @@ def main():
     args = parser.parse_args()
     
     func = {"make_data": make_data.do_make_data, "train": train.do_train, "eval":eval_module.do_eval}[args.func_str]
-    func(args)
+    
+    if args.run_in_pdb:
+        run_in_pdb(func, args)
+#         import pdb
+#         pdb.runcall(func, args)
+    else:
+        func(args)
     
 if __name__ == "__main__":
     main()
