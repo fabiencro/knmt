e205a2183977c7b9b794b73b29e259ee39d5525c
Fabien <fabien.cromieres@gmail.com>
2017-01-20 19:40:56 +0900

    setup.py now storing the git status (hash + diff) when installing

--- nmt_chainer/versioning_tools.py
+++ nmt_chainer/versioning_tools.py
@@ -16,11 +16,31 @@ def get_current_git_diff():
     except:
         return "**Could not retrieve git-diff"
 
+def get_package_git_hash():
+    try:
+        import _build
+        return _build.__build__
+    except:
+        return "**Could not get package git-hash**"
+
+def get_package_git_diff():
+    try:
+        import _build
+        import json
+        return _build.__git_diff__
+    except:
+        return "**Could not get package git-diff**"
 
 def main(options = None):
     import _version
     print "package version:", _version.__version__
     print "installed in:", get_installed_path()
+    
+    print "********** package build info ***********"
+    print "package build (git hash):", get_package_git_hash()
+    print "package build diff (git diff):", get_package_git_diff()
+    
+    print "********** current version info ***********"
     print "git hash:", get_current_git_hash()
     print "git diff:"
     print get_current_git_diff()
