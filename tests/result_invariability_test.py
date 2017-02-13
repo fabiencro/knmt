6fcf346371de87eb725953d2a5ebb8c9a4fb3455
Frederic Bergeron <bergeron@pa.jst.jp>
2017-02-13 13:58:44 +0900

    Added result-invariability test with greedy search.

--- tests/result_invariability_test.py
+++ tests/result_invariability_test.py
@@ -12,10 +12,11 @@ import pytest
 
 class TestResultInvariability:
     
-    def test_result_invariability(self, tmpdir, gpu):
+    def test_result_invariability_using_beam_search(self, tmpdir, gpu):
         """
         Performs some translations with a preexisting model and compare the results
-        with previous results of the same experiment.  The result should be identical.
+        using beam search translation with previous results of the same experiment.  
+        The result should be identical.
         If not, it means that a recent commit have changed the behavior of the system.
         """
         
@@ -35,6 +36,40 @@ class TestResultInvariability:
         with open(os.path.join(str(test_data_dir), "models/result_invariability.translations_using_beam_search.txt")) as f:
             expected_translations = f.readlines()
         with open(beam_search_file) as f:
+            actual_translations = f.readlines()
+        print "expected_translations"
+        for p in expected_translations:
+            print p
+        print "actual_translations"
+        for p in actual_translations:
+            print p
+
+        assert(actual_translations == expected_translations)
+
+    def test_result_invariability_using_greedy_search(self, tmpdir, gpu):
+        """
+        Performs some translations with a preexisting model and compare the results
+        using greedy search translation with previous results of the same experiment.  
+        The result should be identical.
+        If not, it means that a recent commit have changed the behavior of the system.
+        """
+        
+        test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests_data")
+        data_src_file = os.path.join(test_data_dir, "src2.txt")
+        data_tgt_file = os.path.join(test_data_dir, "tgt2.txt")
+        train_dir = os.path.join(test_data_dir, "models")
+        train_prefix = os.path.join(train_dir, "result_invariability.train")
+        greedy_search_eval_dir = tmpdir.mkdir("eval_greedy_search")
+        greedy_search_file = os.path.join(str(greedy_search_eval_dir), 'translations_using_greedy_search.txt')
+        args_eval_greedy_search = [train_prefix + '.train.config', train_prefix + '.model.best.npz', data_src_file, greedy_search_file] + \
+            '--mode translate'.split(' ') 
+        if gpu is not None:
+            args_eval_greedy_search += ['--gpu', gpu]
+        eval.command_line(arguments = args_eval_greedy_search)
+        
+        with open(os.path.join(str(test_data_dir), "models/result_invariability.translations_using_greedy_search.txt")) as f:
+            expected_translations = f.readlines()
+        with open(greedy_search_file) as f:
             actual_translations = f.readlines()
         print "expected_translations"
         for p in expected_translations:
