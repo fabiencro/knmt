9e9ffac86f331d1a1810d58f09431034afe1227c
Frederic Bergeron <bergeron@pa.jst.jp>
2017-03-24 11:46:28 +0900

    Refactored result_invariability tests in only one method.

--- tests/result_invariability_test.py
+++ tests/result_invariability_test.py
@@ -13,85 +13,54 @@ import pytest
 
 class TestResultInvariability:
 
-    @pytest.mark.parametrize("model_name, search_type", [
-        ("result_invariability", "beam_search"),
-        ("result_invariability", "greedy_search"),
-        ("result_invariability_untrained", "beam_search"),
-        ("result_invariability_untrained", "greedy_search")
-    ])
-    def test_result_invariability(self, tmpdir, gpu, model_name, search_type):
-        """
-        Performs some translations with a preexisting models and compare the results
-        using various search types translation with previous results of the same experiment.
-        The results should be identical.
-        If not, it means that a recent commit have changed the behavior of the system.
-        """
-
-        test_data_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "tests_data")
-        data_src_file = os.path.join(test_data_dir, "src2.txt")
-        data_tgt_file = os.path.join(test_data_dir, "tgt2.txt")
-        train_dir = os.path.join(test_data_dir, "models")
-        train_prefix = os.path.join(train_dir, "{0}.train".format(model_name))
-        search_eval_dir = tmpdir.mkdir("eval_{0}".format(search_type))
-        search_file = os.path.join(
-            str(search_eval_dir),
-            'translations_using_{0}.txt'.format(search_type))
-        search_mode = 'beam_search'
-        other_params = ' --beam_width 30'
-        if search_type == 'greedy_search':
-            search_mode = 'translate'
-            other_params = ''
-        args_eval_search = [train_prefix + '.train.config', train_prefix + '.model.best.npz', data_src_file, search_file] + \
-            '--mode {0}{1}'.format(search_mode, other_params).split(' ')
-        if gpu is not None:
-            args_eval_search += ['--gpu', gpu]
-        main(arguments=["eval"] + args_eval_search)
-
-        with open(os.path.join(str(test_data_dir), "models/{0}.translations_using_{1}.txt".format(model_name, search_type))) as f:
-            expected_translations = f.readlines()
-        with open(search_file) as f:
-            actual_translations = f.readlines()
-        print "expected_translations"
-        for p in expected_translations:
-            print p
-        print "actual_translations"
-        for p in actual_translations:
-            print p
-
-        assert(actual_translations == expected_translations)
-
     @pytest.mark.parametrize("model_name, variant_name, variant_options", [
-        ("result_invariability", "prob_space_combination", "--prob_space_combination"),
-        ("result_invariability_untrained", "prob_space_combination", "--prob_space_combination"),
-        ("result_invariability", "google_options_1",
+        ("result_invariability", "beam_search",
+            "--mode beam_search --beam_width 30"),
+        ("result_invariability_untrained", "beam_search",
+            "--mode beam_search --beam_width 30"),
+        ("result_invariability", "greedy_search",
+            "--mode translate"),
+        ("result_invariability_untrained", "greedy_search",
+            "--mode translate"),
+        ("result_invariability", "beam_search_and_prob_space_combination",
+            "--mode beam_search --beam_width 30 "
+            "--prob_space_combination"),
+        ("result_invariability_untrained", "beam_search_and_prob_space_combination",
+            "--mode beam_search --beam_width 30 "
+            "--prob_space_combination"),
+        ("result_invariability", "beam_search_and_google_options_1",
+            "--mode beam_search --beam_width 30 "
             "--beam_pruning_margin 1.5 "
             "--beam_score_coverage_penalty google --beam_score_coverage_penalty_strength 0.3 "
             "--beam_score_length_normalization simple "
             "--post_score_coverage_penalty google --post_score_coverage_penalty_strength 0.4 "
             "--post_score_length_normalization simple"),
-        ("result_invariability_untrained", "google_options_1",
+        ("result_invariability_untrained", "beam_search_and_google_options_1",
+            "--mode beam_search --beam_width 30 "
             "--beam_pruning_margin 1.5 "
             "--beam_score_coverage_penalty google --beam_score_coverage_penalty_strength 0.3 "
             "--beam_score_length_normalization simple "
             "--post_score_coverage_penalty google --post_score_coverage_penalty_strength 0.4 "
             "--post_score_length_normalization simple"),
-        ("result_invariability", "google_options_2",
+        ("result_invariability", "beam_search_and_google_options_2",
+            "--mode beam_search --beam_width 30 "
             "--beam_pruning_margin 1.5 "
             "--beam_score_coverage_penalty google --beam_score_coverage_penalty_strength 0.3 "
             "--beam_score_length_normalization google --beam_score_length_normalization_strength 0.25 "
             "--post_score_coverage_penalty google --post_score_coverage_penalty_strength 0.4 "
             "--post_score_length_normalization google --post_score_length_normalization_strength 0.33"),
-        ("result_invariability_untrained", "google_options_2",
+        ("result_invariability_untrained", "beam_search_and_google_options_2",
+            "--mode beam_search --beam_width 30 "
             "--beam_pruning_margin 1.5 "
             "--beam_score_coverage_penalty google --beam_score_coverage_penalty_strength 0.3 "
             "--beam_score_length_normalization google --beam_score_length_normalization_strength 0.25 "
             "--post_score_coverage_penalty google --post_score_coverage_penalty_strength 0.4 "
             "--post_score_length_normalization google --post_score_length_normalization_strength 0.33")
     ])
-    def test_result_invariability_using_prob_space_combination(self, tmpdir, gpu, model_name, variant_name, variant_options):
+    def test_result_invariability(self, tmpdir, gpu, model_name, variant_name, variant_options):
         """
         Performs some translations with a preexisting models and compare the results
-        using beam_search using different variants with previous results of the same experiment.
+        using different options with previous results of the same experiment.
         The results should be identical.
         If not, it means that a recent commit have changed the behavior of the system.
         """
@@ -101,19 +70,16 @@ class TestResultInvariability:
         data_tgt_file = os.path.join(test_data_dir, "tgt2.txt")
         train_dir = os.path.join(test_data_dir, "models")
         train_prefix = os.path.join(train_dir, "{0}.train".format(model_name))
-        search_eval_dir = tmpdir.mkdir("eval_beam_search")
+        search_eval_dir = tmpdir.mkdir("eval")
         search_file = os.path.join(
             str(search_eval_dir),
-            'translations_using_beam_search_and_{0}.txt'.format(variant_name))
-        search_mode = 'beam_search'
-        other_params = ' --beam_width 30 {0}'.format(variant_options)
-        args_eval_search = [train_prefix + '.train.config', train_prefix + '.model.best.npz', data_src_file, search_file] + \
-            '--mode {0}{1}'.format(search_mode, other_params).split(' ')
+            'translations_using_{0}.txt'.format(variant_name))
+        args_eval_search = [train_prefix + '.train.config', train_prefix + '.model.best.npz', data_src_file, search_file] + variant_options.split(' ')
         if gpu is not None:
             args_eval_search += ['--gpu', gpu]
         main(arguments=["eval"] + args_eval_search)
 
-        with open(os.path.join(str(test_data_dir), "models/{0}.translations_using_beam_search_and_{1}.txt".format(model_name, variant_name))) as f:
+        with open(os.path.join(str(test_data_dir), "models/{0}.translations_using_{1}.txt".format(model_name, variant_name))) as f:
             expected_translations = f.readlines()
         with open(search_file) as f:
             actual_translations = f.readlines()
