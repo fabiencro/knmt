7a8cdb661b925948440c2535ccf2323c46d64811
Frederic Bergeron <bergeron@pa.jst.jp>
2017-05-22 14:41:17 +0900

    Added script to flatten results using --nbest option.

---
+++ nmt_chainer/utilities/flatten_nbest_output.py
@@ -0,0 +1,16 @@
+"""flatten_nbest_output.py: Extract the first sentence of a translation result file that used --nbest."""
+
+import fileinput
+import re
+
+p = re.compile("^(\d+) \|\|\| (.*)$")
+
+sentence_idx = -1
+for line in fileinput.input():
+    m = p.match(line)
+    if m:
+        line_sentence_idx = m.group(1)
+        line_sentence_text = m.group(2)
+        if line_sentence_idx != sentence_idx:
+            print line_sentence_text
+            sentence_idx = line_sentence_idx
