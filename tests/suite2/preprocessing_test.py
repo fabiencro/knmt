11a9a06da37b32d47272498d66b7dfe5620fbadf
Frederic Bergeron <bergeron@pa.jst.jp>
2017-06-06 10:44:58 +0900

    Added tests for BiProcessChain.

--- tests/suite2/preprocessing_test.py
+++ tests/suite2/preprocessing_test.py
@@ -18,9 +18,9 @@ class TestLatinScriptProcess:
         pp = processors.LatinScriptProcess()
 
         experiments = {
-            "Hello": u"{0}hello".format(processors.LatinScriptProcess.CAP_CHAR),
-            "HELLO": u"{0}hello".format(processors.LatinScriptProcess.ALL_CAPS_CHAR),
-            "HeLLo": u"HeLLo"
+            u"Hello": u"{0}hello".format(processors.LatinScriptProcess.CAP_CHAR),
+            u"HELLO": u"{0}hello".format(processors.LatinScriptProcess.ALL_CAPS_CHAR),
+            u"HeLLo": u"HeLLo"
         }
 
         for k, v in experiments.items():
@@ -41,9 +41,9 @@ class TestLatinScriptProcess:
     def test_convert_deconvert_caps_alt(self):
         pp = processors.LatinScriptProcess()
 
-        s1 = "Hello"
-        s2 = "HELLO"
-        s3 = "HeLLo"
+        s1 = u"Hello"
+        s2 = u"HELLO"
+        s3 = u"HeLLo"
         t1 = pp.convert_caps_alt(s1)
         t2 = pp.convert_caps_alt(s2)
         t3 = pp.convert_caps_alt(s3)
@@ -76,20 +76,19 @@ class TestLatinScriptProcess:
         pp = processors.LatinScriptProcess()
 
         experiments = {
-            "x...": u"x {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Wonderful!": u"Wonderful {0}!".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Really?": u"Really {0}?".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "def:": u"def {0}:".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "a,": u"a {0},".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "b;": u"b {0};".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "6\"": u"6 {0}\"".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "100%": u"100 {0}%".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "1000000$": u"1000000 {0}$".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Chris'": u"Chris {0}'".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Back`": u"Back {0}`".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Paren)": u"Paren {0})".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Square]": u"Square {0}]".format(processors.LatinScriptProcess.SUFFIX_CHAR)
-
+            u"x...": u"x {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Wonderful!": u"Wonderful {0}!".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Really?": u"Really {0}?".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"def:": u"def {0}:".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"a,": u"a {0},".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"b;": u"b {0};".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"6\"": u"6 {0}\"".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"100%": u"100 {0}%".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"1000000$": u"1000000 {0}$".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Chris'": u"Chris {0}'".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Back`": u"Back {0}`".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Paren)": u"Paren {0})".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Square]": u"Square {0}]".format(processors.LatinScriptProcess.SUFFIX_CHAR)
         }
 
         for k, v in experiments.items():
@@ -106,20 +105,20 @@ class TestLatinScriptProcess:
         pp = processors.LatinScriptProcess()
 
         experiments = {
-            "...": u"...",
-            "maybe...": u"maybe {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            ".test.star": u".test {0}.star".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "!test!star!wow": u"!test {0}!star {0}!wow".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "%test%star%wow...": u"%test {0}%star {0}%wow {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test+one": u"test {0}+one".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test?two": u"test {0}?two".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test?three": u"test {0}?three".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test'four": u"test {0}'four".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test\"five": u"test {0}\"five".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "test%six!abc": u"test {0}%six {0}!abc".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "sentence_with!all:the;chars$that]we(want'to&test<if/it@is|possible.":
+            u"...": u"...",
+            u"maybe...": u"maybe {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u".test.star": u".test {0}.star".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"!test!star!wow": u"!test {0}!star {0}!wow".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"%test%star%wow...": u"%test {0}%star {0}%wow {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test+one": u"test {0}+one".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test?two": u"test {0}?two".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test?three": u"test {0}?three".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test'four": u"test {0}'four".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test\"five": u"test {0}\"five".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"test%six!abc": u"test {0}%six {0}!abc".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"sentence_with!all:the;chars$that]we(want'to&test<if/it@is|possible.":
                 u"sentence {0}_with {0}!all {0}:the {0};chars {0}$that {0}]we {0}(want {0}'to {0}&test {0}<if {0}/it {0}@is {0}|possible {0}.".format(processors.LatinScriptProcess.SUFFIX_CHAR),
-            "sentence#with^all<the>other=chars*that\\we[want@to-test":
+            u"sentence#with^all<the>other=chars*that\\we[want@to-test":
                 u"sentence {0}#with {0}^all {0}<the {0}>other {0}=chars {0}*that {0}\\we {0}[want {0}@to {0}-test".format(processors.LatinScriptProcess.SUFFIX_CHAR)
         }
 
@@ -137,9 +136,9 @@ class TestLatinScriptProcess:
         pp = processors.LatinScriptProcess()
 
         experiments = {
-            "...": u"...",
-            "This  is     a    whitespace    test.": u"{0}this is a whitespace test {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Let's check IT out!!!": u"{0}let {1}'s check {2}it out {1}! {1}! {1}!".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR, processors.LatinScriptProcess.ALL_CAPS_CHAR),
+            u"...": u"...",
+            u"This  is     a    whitespace    test.": u"{0}this is a whitespace test {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Let's check IT out!!!": u"{0}let {1}'s check {2}it out {1}! {1}! {1}!".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR, processors.LatinScriptProcess.ALL_CAPS_CHAR),
             u"Éric est parti à l'école très tôt.": u"{0}éric est parti à l {1}'école très tôt {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR)
         }
 
@@ -167,9 +166,9 @@ class TestLatinScriptProcess:
         pp = processors.LatinScriptProcess(mode="caps_isolate")
 
         experiments = {
-            "...": u"...",
-            "This  is     a    whitespace    test.": u"{0} this is a whitespace test {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR),
-            "Let's check IT out!!!": u"{0} let {1}'s check {2} it out {1}! {1}! {1}!".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR, processors.LatinScriptProcess.ALL_CAPS_CHAR),
+            u"...": u"...",
+            u"This  is     a    whitespace    test.": u"{0} this is a whitespace test {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR),
+            u"Let's check IT out!!!": u"{0} let {1}'s check {2} it out {1}! {1}! {1}!".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR, processors.LatinScriptProcess.ALL_CAPS_CHAR),
             u"Éric est parti à l'école très tôt.": u"{0} éric est parti à l {1}'école très tôt {1}.".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR)
         }
 
@@ -200,10 +199,10 @@ class TestSimpleSegmenter:
         pp = processors.SimpleSegmenter()
 
         experiments = {
-            "This is a test sentence.": ['This', 'is', 'a', 'test', 'sentence.'],
-            "   This    is  a test    sentence   with  white    spaces  ...":
+            u"This is a test sentence.": ['This', 'is', 'a', 'test', 'sentence.'],
+            u"   This    is  a test    sentence   with  white    spaces  ...":
                 ['', '', '', 'This', '', '', '', 'is', '', 'a', 'test', '', '', '', 'sentence', '', '', 'with', '', 'white', '', '', '', 'spaces', '', '...'],
-            "What HAPPEN if there's funny chars like !@#$%?": ['What', 'HAPPEN', 'if', "there's", 'funny', 'chars', 'like', '!@#$%?']
+            u"What HAPPEN if there's funny chars like !@#$%?": ['What', 'HAPPEN', 'if', "there's", 'funny', 'chars', 'like', '!@#$%?']
         }
 
         for k, v in experiments.items():
@@ -215,10 +214,10 @@ class TestSimpleSegmenter:
         pp = processors.SimpleSegmenter(type="word2char")
 
         experiments = {
-            "This is a test sentence.": ('T', 'h', 'i', 's', 'i', 's', 'a', 't', 'e', 's', 't', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '.'),
-            "   This    is  a test    sentence   with  white    spaces  ...":
+            u"This is a test sentence.": ('T', 'h', 'i', 's', 'i', 's', 'a', 't', 'e', 's', 't', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '.'),
+            u"   This    is  a test    sentence   with  white    spaces  ...":
                 ('T', 'h', 'i', 's', 'i', 's', 'a', 't', 'e', 's', 't', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', 'w', 'i', 't', 'h', 'w', 'h', 'i', 't', 'e', 's', 'p', 'a', 'c', 'e', 's', '.', '.', '.'),
-            "What HAPPEN if there's funny chars like !@#$%?": ('W', 'h', 'a', 't', 'H', 'A', 'P', 'P', 'E', 'N', 'i', 'f', 't', 'h', 'e', 'r', 'e', "'", 's', 'f', 'u', 'n', 'n', 'y', 'c', 'h', 'a', 'r', 's', 'l', 'i', 'k', 'e', '!', '@', '#', '$', '%', '?')
+            u"What HAPPEN if there's funny chars like !@#$%?": ('W', 'h', 'a', 't', 'H', 'A', 'P', 'P', 'E', 'N', 'i', 'f', 't', 'h', 'e', 'r', 'e', "'", 's', 'f', 'u', 'n', 'n', 'y', 'c', 'h', 'a', 'r', 's', 'l', 'i', 'k', 'e', '!', '@', '#', '$', '%', '?')
         }
 
         for k, v in experiments.items():
@@ -230,10 +229,10 @@ class TestSimpleSegmenter:
         pp = processors.SimpleSegmenter(type="char")
 
         experiments = {
-            "This is a test sentence.": ('T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', ' ', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '.'),
-            "   This    is  a test    sentence   with  white    spaces  ...":
+            u"This is a test sentence.": ('T', 'h', 'i', 's', ' ', 'i', 's', ' ', 'a', ' ', 't', 'e', 's', 't', ' ', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', '.'),
+            u"   This    is  a test    sentence   with  white    spaces  ...":
                 (' ', ' ', ' ', 'T', 'h', 'i', 's', ' ', ' ', ' ', ' ', 'i', 's', ' ', ' ', 'a', ' ', 't', 'e', 's', 't', ' ', ' ', ' ', ' ', 's', 'e', 'n', 't', 'e', 'n', 'c', 'e', ' ', ' ', ' ', 'w', 'i', 't', 'h', ' ', ' ', 'w', 'h', 'i', 't', 'e', ' ', ' ', ' ', ' ', 's', 'p', 'a', 'c', 'e', 's', ' ', ' ', '.', '.', '.'),
-            "What HAPPEN if there's funny chars like !@#$%?":
+            u"What HAPPEN if there's funny chars like !@#$%?":
                 ('W', 'h', 'a', 't', ' ', 'H', 'A', 'P', 'P', 'E', 'N', ' ', 'i', 'f', ' ', 't', 'h', 'e', 'r', 'e', "'", 's', ' ', 'f', 'u', 'n', 'n', 'y', ' ', 'c', 'h', 'a', 'r', 's', ' ', 'l', 'i', 'k', 'e', ' ', '!', '@', '#', '$', '%', '?')
         }
 
@@ -252,10 +251,10 @@ class TestProcessorChain:
         pp = processors.ProcessorChain([pp_1, pp_2])
 
         experiments = {
-            "Hello": (processors.LatinScriptProcess.CAP_CHAR, u'h', u'e', u'l', u'l', u'o'),
-            "HELLO": (processors.LatinScriptProcess.ALL_CAPS_CHAR, u'h', u'e', u'l', u'l', u'o'),
-            "HeLLo": (u'H', u'e', u'L', u'L', u'o'),
-            "sentence_with!all:the;chars$that]we(want'to&test<if/it@is|possible.":
+            u"Hello": (processors.LatinScriptProcess.CAP_CHAR, u'h', u'e', u'l', u'l', u'o'),
+            u"HELLO": (processors.LatinScriptProcess.ALL_CAPS_CHAR, u'h', u'e', u'l', u'l', u'o'),
+            u"HeLLo": (u'H', u'e', u'L', u'L', u'o'),
+            u"sentence_with!all:the;chars$that]we(want'to&test<if/it@is|possible.":
                 (u's', u'e', u'n', u't', u'e', u'n', u'c', u'e', u' ',
                  processors.LatinScriptProcess.SUFFIX_CHAR, u'_', u'w', u'i', u't', u'h', u' ',
                  processors.LatinScriptProcess.SUFFIX_CHAR, u'!', u'a', u'l', u'l', u' ',
@@ -292,3 +291,63 @@ class TestProcessorChain:
             pp.convert(u"in{0}side".format(processors.LatinScriptProcess.SUFFIX_CHAR))
         except Exception, ex:
             assert type(ex) == ValueError and str(ex) == "Special char in word"
+
+
+class TestBiProcessChain:
+
+    def test_convert_deconvert_with_simple_processors(self):
+        pp = processors.BiProcessorChain()
+
+        src_pp = processors.LatinScriptProcess(mode="all_adjoint")
+        pp.add_src_processor(src_pp)
+
+        tgt_pp = processors.LatinScriptProcess(mode="all_adjoint")
+        pp.add_tgt_processor(tgt_pp)
+
+        experiments = {
+            (u"Hello", u"HELLO", 
+             u"{0}hello".format(processors.LatinScriptProcess.CAP_CHAR), u"{0}hello".format(processors.LatinScriptProcess.ALL_CAPS_CHAR)),
+            (u"x...", u"Wonderful!",
+             u"x {0}...".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+             u"{0}wonderful {1}!".format(processors.LatinScriptProcess.CAP_CHAR, processors.LatinScriptProcess.SUFFIX_CHAR)),
+            (u"test?three", u"test'four",
+             u"test {0}?three".format(processors.LatinScriptProcess.SUFFIX_CHAR), 
+             u"test {0}'four".format(processors.LatinScriptProcess.SUFFIX_CHAR)),
+            (u"sentence_with!all:the;chars$that]we(want'to&test<if/it@is|possible.",
+             u"sentence#with^all<the>other=chars*that\\we[want@to-test",
+             u"sentence {0}_with {0}!all {0}:the {0};chars {0}$that {0}]we {0}(want {0}'to {0}&test {0}<if {0}/it {0}@is {0}|possible {0}.".format(processors.LatinScriptProcess.SUFFIX_CHAR),
+             u"sentence {0}#with {0}^all {0}<the {0}>other {0}=chars {0}*that {0}\\we {0}[want {0}@to {0}-test".format(processors.LatinScriptProcess.SUFFIX_CHAR))
+
+        }
+
+        for idx, exp_data in enumerate(experiments):
+            sentence_src = exp_data[0]
+            sentence_tgt = exp_data[1]
+            sentence_src_expected_res = exp_data[2]
+            sentence_tgt_expected_res = exp_data[3]
+            sentence_src_res, sentence_tgt_res = pp.convert(sentence_src, sentence_tgt)
+            # print u"Exp {0}".format(idx)
+            # print u"sentence_src={0}".format(sentence_src)
+            # print u"sentence_tgt={0}".format(sentence_tgt)
+            # print u"sentence_src_res={0}".format(sentence_src_res)
+            # print u"sentence_tgt_res={0}".format(sentence_tgt_res)
+            assert sentence_src_res == sentence_src_expected_res 
+            assert sentence_tgt_res == sentence_tgt_expected_res
+            
+            sentence_src_res, sentence_tgt_res = pp.deconvert(sentence_src_res, sentence_tgt_res)
+            assert sentence_src_res == sentence_src
+            assert sentence_tgt_res == sentence_tgt
+
+        valid_sentence = u"..."
+        invalid_sentence = u"in{0}side".format(processors.LatinScriptProcess.SUFFIX_CHAR)
+
+        try:
+            sentence_src_res, sentence_tgt_res = pp.convert(invalid_sentence, valid_sentence)
+        except Exception, ex:
+            assert type(ex) == ValueError and str(ex) == "Special char in word"
+
+        try:
+            sentence_src_res, sentence_tgt_res = pp.convert(valid_sentence, invalid_sentence)
+        except Exception, ex:
+            assert type(ex) == ValueError and str(ex) == "Special char in word"
+
