a067db8dce3c972090bcbe0202db92c624d1b80d
nakao <nakario.jp@gmail.com>
2017-06-12 14:38:45 +0900

    Add word level edit distance similarity

--- nmt_chainer/search_engine/similarity.py
+++ nmt_chainer/search_engine/similarity.py
@@ -1,5 +1,11 @@
-from Levenshtein import distance
+from nltk import edit_distance
 
 
 def fuzzy_char_level_similarity(x, y):
-    return 1 - distance(x, y) / max(len(x), len(y))
+    return 1 - edit_distance(x, y) * 1.0 / max(len(x), len(y))
+
+
+def fuzzy_word_level_similarity(x, y, sep=None):
+    xs = x.split(sep)
+    ys = y.split(sep)
+    return fuzzy_char_level_similarity(xs, ys)
