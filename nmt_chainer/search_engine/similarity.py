a2188cd1ea56aa3563ca58a980f830ada8d5efe8
nakao <nakario.jp@gmail.com>
2017-06-13 12:09:28 +0900

    Make function's implementation more clear

--- nmt_chainer/search_engine/similarity.py
+++ nmt_chainer/search_engine/similarity.py
@@ -8,4 +8,4 @@ def fuzzy_char_level_similarity(x, y):
 def fuzzy_word_level_similarity(x, y, sep=None):
     xs = x.split(sep)
     ys = y.split(sep)
-    return fuzzy_char_level_similarity(xs, ys)
+    return 1 - edit_distance(xs, ys) * 1.0 / max(len(xs), len(ys))
