993e09c2b41d2fcdd62d372e629bc46d0bfcd2c9
Fabien <fabien.cromieres@gmail.com>
2017-06-28 02:39:53 +0900

    Fixed bug in multi attention

--- nmt_chainer/models/feedforward/encoder_decoder.py
+++ nmt_chainer/models/feedforward/encoder_decoder.py
@@ -19,6 +19,8 @@ class EncoderDecoder(Chain):
         
         self.V_tgt = V_tgt
         
+    
+        
     def compute_loss(self, src_seq, tgt_seq, train=True):
         encoded_source, src_mask = self.encoder(src_seq, train=train)
         loss = self.decoder.compute_loss(tgt_seq, encoded_source, src_mask, train=train)
@@ -29,7 +31,6 @@ class EncoderDecoder(Chain):
         decoding_cell = self.decoder.get_conditionalized_cell(encoded_source, src_mask)
         
         logits, decoder_state = decoding_cell.get_initial_logits(train=False)
-        prev_word = None
         
         mb_size = len(src_seq)
         result = [[] for _ in xrange(mb_size)]
@@ -50,6 +51,73 @@ class EncoderDecoder(Chain):
             logits, decoder_state = decoding_cell(decoder_state, prev_word, train=False)
         return result
             
+    def compute_logits(self, src_seq, tgt_seq, train=True):
+        encoded_source, src_mask = self.encoder(src_seq, train=train)
+        logits = self.decoder.compute_logits(tgt_seq, encoded_source, src_mask, train=train)
+        return logits
+      
+    def compute_logits_step_by_step(self, src_seq, tgt_seq, train=True):
+        encoded_source, src_mask = self.encoder(src_seq, train=train)
+        decoding_cell = self.decoder.get_conditionalized_cell(encoded_source, src_mask)
+        
+        logits, decoder_state = decoding_cell.get_initial_logits(train=train)            
+        
+        from nmt_chainer.models.feedforward.utils import pad_data
+        padded_tgt = pad_data(tgt_seq, pad_value=0)#, add_eos=self.decoder.eos_idx)
+        
+        max_tgt_length = padded_tgt.shape[1]
+        seq_padded_tgt = [padded_tgt[:, i:i+1] for i in range(max_tgt_length)]
+        
+#         loss = F.softmax_cross_entropy(F.reshape(logits, (-1, self.decoder.V+1)), padded_target_with_eos.reshape(-1,))
+        
+        mb_size = len(src_seq)
+        result = [logits]
+        
+        
+        for num_step in range(max_tgt_length):
+#             print "logits shape", logits.shape
+            prev_word = seq_padded_tgt[num_step]
+#             print "prev w shape", prev_word.shape
+            assert prev_word.shape == (mb_size, 1)
             
-    
+            logits, decoder_state = decoding_cell(decoder_state, prev_word, train=train)
+            result.append(logits)
+            
+        return result  
+            
+    def compute_loss_step_by_step(self, src_seq, tgt_seq, train=True):
+        encoded_source, src_mask = self.encoder(src_seq, train=train)
+        decoding_cell = self.decoder.get_conditionalized_cell(encoded_source, src_mask)
+        
+        logits, decoder_state = decoding_cell.get_initial_logits(train=train)            
+        
+        from nmt_chainer.models.feedforward.utils import pad_data
+        padded_tgt = pad_data(tgt_seq, pad_value=0)#, add_eos=self.decoder.eos_idx)
+        
+        max_tgt_length = padded_tgt.shape[1]
+        seq_padded_tgt = [padded_tgt[:, i] for i in range(max_tgt_length)]
+        
+        padded_target_with_eos = pad_data(tgt_seq, pad_value=-1, add_eos=self.decoder.eos_idx)
+        
+#         loss = F.softmax_cross_entropy(F.reshape(logits, (-1, self.decoder.V+1)), padded_target_with_eos.reshape(-1,))
+        
+        mb_size = len(src_seq)
+        result = [[] for _ in xrange(mb_size)]
+        
+        num_step = 0
+        while 1:
+#             print "logits shape", logits.shape
+            prev_word = padded_tgt[num_step]
+#             print "prev w shape", prev_word.shape
+            assert prev_word.shape == (mb_size, 1)
+            for i in xrange(mb_size):
+                result[i].append(prev_word[i, 0])
+            
+            prev_word = self.xp.where(prev_word == self.decoder.eos_idx, 0, prev_word)
+            num_step += 1
+            if num_step >= max_tgt_length:
+                break
+            logits, decoder_state = decoding_cell(decoder_state, prev_word, train=train)
+        return result    
+        
         
