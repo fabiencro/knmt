fb3109614908552ec1ee516ed4c1f35dc61072af
Frederic Bergeron <bergeron@pa.jst.jp>
2017-09-05 17:47:36 +0900

    Handled text splitting and sentence translation requests.

--- nmt_chainer/translation/multiserver.py
+++ nmt_chainer/translation/multiserver.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """multiserver.py: Translation server that works asynchronously and dispatches requests to multiple translation servers."""
 __author__ = "Frederic Bergeron"
 __license__ = "undecided"
@@ -18,7 +19,11 @@ import socket
 import threading
 import SocketServer
 
+from urllib import urlencode
+import requests
 import Queue
+
+from nmt_chainer.translation.client import Client
 
 logging.basicConfig()
 log = logging.getLogger("rnns:server")
@@ -26,11 +31,13 @@ log.setLevel(logging.INFO)
 
 class Worker(threading.Thread):
 
-    def __init__(self, name, src_lang, tgt_lang, manager):
+    def __init__(self, name, src_lang, tgt_lang, host, port, manager):
         threading.Thread.__init__(self)
         self.name = name
         self.src_lang = src_lang
         self.tgt_lang = tgt_lang
+        self.host = host
+        self.port = port
         self.manager = manager
 
     def run(self):
@@ -39,7 +46,47 @@ class Worker(threading.Thread):
             translation_request = self.manager.translation_request_queues[key].get(True)
             start_request = timeit.default_timer()
             log.info(timestamped_msg("Thead {0}: Handling request: {1}:{2}".format(self.name, translation_request['session_id'], translation_request['article_id'])))
-            sleep(60)
+
+            if 'text' in translation_request:
+                log.info("TEXT: {0} {1}".format(type(translation_request['text']), translation_request['text'].encode('utf-8')))
+                data = {
+                    'text': translation_request['text'],
+                    'lang_source': translation_request['src_lang'],
+                    'lang_target': translation_request['tgt_lang']
+                }
+                splitter_url = 'http://lotus.kuee.kyoto-u.ac.jp/~frederic/webmt-rnnsearch-multiple-requests/cgi-bin/split_sentences.cgi'
+                r = requests.post(splitter_url, data)
+                if r.status_code == 200:
+                    json_resp = r.json()
+                    for index, sentence in enumerate(json_resp['sentences']):
+                        translate_sentence_request = dict(translation_request)
+                        del translate_sentence_request['text']
+                        translate_sentence_request['sentence'] = sentence
+                        translate_sentence_request['sentence_number'] = index
+                        translate_sentence_request['lang_source'] = self.src_lang
+                        translate_sentence_request['lang_target'] = self.tgt_lang
+                        log.info("new_req={0}".format(translate_sentence_request))
+                        self.manager.translation_request_queues[key].put(translate_sentence_request)
+            elif 'sentence' in translation_request:
+                log.info("SENTENCE for worker {0}:{1}: {2} {3}".format(self.host, self.port, type(translation_request['sentence']), translation_request['sentence'].encode('utf-8')))
+                client = Client(self.host, self.port)
+                resp = client.query(translation_request['sentence'].encode('utf-8'), 
+                    article_id=translation_request['article_id'],
+                    beam_width=translation_request['beam_width'],
+                    nb_steps=translation_request['nb_steps'],
+                    nb_steps_ratio=translation_request['nb_steps_ratio'],
+                    prob_space_combination=translation_request['prob_space_combination'],
+                    normalize_unicode_unk=translation_request['normalize_unicode_unk'],
+                    remove_unk=translation_request['remove_unk'],
+                    attempt_to_relocate_unk_source=translation_request['attempt_to_relocate_unk_source'],
+                    sentence_id=translation_request['sentence_number'])
+                #log.info("resp={0}".format(resp))
+                json_resp = json.loads(resp)
+                if 'out' in json_resp:
+                    log.info("TRANSLATION: {0}".format(json_resp['out'].encode('utf-8')))
+                else:
+                    log.info("RESPONSE: {0}".format(json_resp))
+
             log.info("Request processed in {0} s. by {1} [{2}:{3}]".format(timeit.default_timer() - start_request, self.name, translation_request['session_id'], translation_request['article_id']))
 
 class Manager(object):
@@ -54,7 +101,7 @@ class Manager(object):
             workerz = []
             self.workers[key] = workerz
             for idx, server in enumerate(translation_server_config[key]):
-                worker = Worker("Translater-{0}({1}-{2})".format(idx, src_lang, tgt_lang), src_lang, tgt_lang, self)
+                worker = Worker("Translater-{0}({1}-{2})".format(idx, src_lang, tgt_lang), src_lang, tgt_lang, server['host'], server['port'], self)
                 workerz.append(worker)
                 worker.start()
         log.info("qs={0}".format(self.translation_request_queues))
