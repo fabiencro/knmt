eec42ebd7ab2892d6bb04d8670bc2b88ebbb98dc
Frederic Bergeron <bergeron@pa.jst.jp>
2017-09-08 13:04:40 +0900

    First attempt to distribute the load of the request.

--- nmt_chainer/translation/multiserver.py
+++ nmt_chainer/translation/multiserver.py
@@ -18,6 +18,7 @@ import timeit
 import socket
 import threading
 import SocketServer
+from collections import deque
 
 from urllib import urlencode
 import requests
@@ -28,6 +29,53 @@ from nmt_chainer.translation.client import Client
 logging.basicConfig()
 log = logging.getLogger("rnns:server")
 log.setLevel(logging.INFO)
+
+TEXT_REQ_PRIORITY = 100
+SENTENCE_REQ_PRIORITY = 2000
+
+class RequestQueue(Queue.PriorityQueue):
+
+    def __init__(self):
+        Queue.Queue.__init__(self)
+
+    def redistribute_requests(self):
+        log.info("redistribute_requests begin")
+        self.mutex.acquire()
+        temp = {}
+        log.info('begin')
+        while self._qsize() > 0:
+            priority, item = self._get()
+	    #log.info("priority={0} item={1}".format(priority, item))
+            #log.info("{0} {1}".format(item['session_id'], item['msg_id']))
+            #log.info("{0} {1}".format(item['session_id'], item['article_id']))
+            if 'sentence_number' in item:
+                log.info("{0} {1} {2}".format(priority, item['session_id'], item['sentence_number']))
+            elif 'type' in item:
+                log.info("{0} {1} {2}".format(priority, item['session_id'], item['type']))
+            if not item['session_id'] in temp:
+                temp[item['session_id']] = deque()
+            temp[item['session_id']].append((priority, item))
+        log.info('end')
+        keys = temp.keys()[:]
+	log.info('keys={0}'.format(keys))
+        log.info('begin2')
+	r = 0
+        while keys:
+            for k in keys:
+                priority, item = temp[k].popleft()
+		new_priority = SENTENCE_REQ_PRIORITY + r
+		r += 1
+                if 'sentence_number' in item:
+                    log.info("{0} {1} {2}".format(new_priority, item['session_id'], item['sentence_number']))
+                elif 'type' in item:
+                    log.info("{0} {1} {2}".format(new_priority, item['session_id'], item['type']))
+		#log.info("priority={0} item={1}".format(new_priority, item))
+                self._put((new_priority, item))
+            keys = [k for k in keys if temp[k]]
+        log.info('end2')
+        self.mutex.release()
+        log.info("redistribute_requests end")
+
 
 class Worker(threading.Thread):
 
@@ -41,14 +89,15 @@ class Worker(threading.Thread):
         self.manager = manager
 
     def run(self):
+	key = "{0}-{1}".format(self.src_lang, self.tgt_lang)
         while True:
-            key = "{0}-{1}".format(self.src_lang, self.tgt_lang)
-            translation_request = self.manager.translation_request_queues[key].get(True)
+            request_priority, translation_request = self.manager.translation_request_queues[key].get(True)
+            log.info("request={0}".format(translation_request))
             start_request = timeit.default_timer()
-            log.info(timestamped_msg("Thead {0}: Handling request: {1}:{2}".format(self.name, translation_request['session_id'], translation_request['article_id'])))
+            log.info(timestamped_msg("Thread {0}: Handling request: {1}:{2}".format(self.name, translation_request['session_id'], translation_request['article_id'])))
 
             if 'text' in translation_request:
-                log.info("TEXT: {0} {1}".format(type(translation_request['text']), translation_request['text'].encode('utf-8')))
+                log.info("TEXT: {0} {1}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') ,translation_request['text'].encode('utf-8')))
                 data = {
                     'text': translation_request['text'],
                     'lang_source': translation_request['src_lang'],
@@ -67,6 +116,7 @@ class Worker(threading.Thread):
                         self.manager.client_responses[translation_request['session_id']] = response_queue
                     response_queue.put(json_resp)
 
+                    log.info("sentenceCount={0}".format(len(json_resp['sentences'])))
                     for index, sentence in enumerate(json_resp['sentences']):
                         translate_sentence_request = dict(translation_request)
                         del translate_sentence_request['text']
@@ -75,9 +125,12 @@ class Worker(threading.Thread):
                         translate_sentence_request['lang_source'] = self.src_lang
                         translate_sentence_request['lang_target'] = self.tgt_lang
                         log.info("new_req={0}".format(translate_sentence_request))
-                        self.manager.translation_request_queues[key].put(translate_sentence_request)
+                        self.manager.translation_request_queues[key].put((SENTENCE_REQ_PRIORITY, translate_sentence_request))
+                    log.info("q before sz={0}".format(self.manager.translation_request_queues[key].qsize()))
+                    self.manager.translation_request_queues[key].redistribute_requests()
+                    log.info("q after sz={0}".format(self.manager.translation_request_queues[key].qsize()))
             elif 'sentence' in translation_request:
-                log.info("SENTENCE for worker {0}:{1}: {2} {3}".format(self.host, self.port, type(translation_request['sentence']), translation_request['sentence'].encode('utf-8')))
+                log.info("SENTENCE for worker {0}:{1}: {2}".format(self.host, self.port, translation_request['sentence'].encode('utf-8')))
                 client = Client(self.host, self.port)
                 resp = client.query(translation_request['sentence'].encode('utf-8'), 
                     article_id=translation_request['article_id'],
@@ -109,7 +162,8 @@ class Manager(object):
         self.workers = {}
         for key in translation_server_config:
             src_lang, tgt_lang = key.split("-")
-            self.translation_request_queues[key] = Queue.Queue()
+            #self.translation_request_queues[key] = Queue.Queue()
+            self.translation_request_queues[key] = RequestQueue()
             workerz = []
             self.workers[key] = workerz
             for idx, server in enumerate(translation_server_config[key]):
@@ -146,13 +200,18 @@ class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                 log.info("data={0}".format(str_data))
                 json_data = json.loads(str_data)
                 
+                response = {}
                 key = "{0}-{1}".format(json_data['src_lang'], json_data['tgt_lang'])
                 if json_data['type'] == 'translate':
-                    self.manager.translation_request_queues[key].put(json_data)
+                    log.info("TRANSLATE!!!! {0} from {1}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S'), json_data['session_id']))
+                    self.manager.translation_request_queues[key].put((TEXT_REQ_PRIORITY, json_data))
+                    # self.manager.translation_request_queues[key].redistribute_requests()
                     response = {'msg': 'Translation request has been added to queue.'}
                 elif json_data['type'] == 'poll':
+		    log.info("POLL from {0}".format(json_data['session_id']))	
                     response = self.manager.poll(json_data['session_id'])
                 elif json_data['type'] == 'debug':
+                    log.info('debug!')
                     log.info(self.manager.translation_request_queues)
                     trans_queues = {}
                     for k, q in list(self.manager.translation_request_queues.items()):
@@ -164,10 +223,9 @@ class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                     }
                 else:
                     response = {'msg': 'Unknown request type. Request has been ignored.'}
-                    pass
+
                 log.info("q={0} sz={1}".format(self.manager.translation_request_queues[key], self.manager.translation_request_queues[key].qsize()))
-                log.info("Request processed in {0} s. by {1}".format(
-                    timeit.default_timer() - start_request, threading.current_thread().name))
+                log.info("Request processed in {0} s. by {1}".format(timeit.default_timer() - start_request, threading.current_thread().name))
                 response = json.dumps(response)
                 self.request.sendall(response)
 
@@ -199,3 +257,100 @@ def do_start_server(config_file):
         server.server_close()
 
     sys.exit(0)
+
+
+class ProducerWorker(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        global z
+        log.info("Producer started.")
+        while True:
+            for i in range(4):
+                q.put(({'session_id': z, 'msg_id': "{0}_{1}".format(z, i)}))
+            z += 1
+            q.redistribute_requests()
+            sleep(5)
+
+class ConsumerWorker(threading.Thread):
+
+    def __init__(self):
+        threading.Thread.__init__(self)
+
+    def run(self):
+        log.info("Consumer started.")
+        while True:
+            priority, item = q.get(True)
+            log.info("Processing item={0}".format(item['msg_id']))
+            sleep(4)
+
+if __name__ == "__main__":
+    z = 1
+    q = RequestQueue()
+
+    #p = ProducerWorker()
+    c = ConsumerWorker()
+    # p.start()
+    # c.start()
+    # p.join()
+    # c.join()
+
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 1, 'msg_id': '1_1'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 1, 'msg_id': '1_2'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 1, 'msg_id': '1_3'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 1, 'msg_id': '1_4'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 2, 'msg_id': '2_1'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 2, 'msg_id': '2_2'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 2, 'msg_id': '2_3'}))
+    q.put((SENTENCE_REQ_PRIORITY, {'session_id': 2, 'msg_id': '2_4'}))
+    q.redistribute_requests()
+    #p.start()
+    c.start()
+    #p.join()
+    c.join()
+
+    log.info(q)
+    # q.redistribute_requests()
+
+# if __name__ == "__main__":
+#     # q = Queue.PriorityQueue()
+#     # q.put((TEXT_REQ_PRIORITY, {'type': 'text', 'id': 't1'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's1_1'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's1_2'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's1_3'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's1_4'}))
+# 
+#     # q.put((TEXT_REQ_PRIORITY, {'type': 'text', 'id': 't2'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's2_1'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's2_2'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's2_3'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's2_4'}))
+# 
+#     # q.put((TEXT_REQ_PRIORITY, {'type': 'text', 'id': 't3'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's3_1'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's3_2'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's3_3'}))
+#     # q.put((SENTENCE_REQ_PRIORITY, {'type': 'sentence', 'id': 's3_4'}))
+# 
+#     # while not q.empty():
+#     #     item = q.get()
+#     #     print item
+# 
+# 
+#     # q = Queue.PriorityQueue()
+#     # # q.put(10)
+#     # # q.put(1)
+#     # # q.put(5)
+#     # # q.put('dix', 10)
+#     # # q.put('un', 1)
+#     # # q.put('cinq', 5)
+#     # # q.put(('dix', 10))
+#     # # q.put(('un', 1))
+#     # # q.put(('cinq', 5))
+#     # q.put((10, 'dix'))
+#     # q.put((1, 'un'))
+#     # q.put((5, 'cinq'))
+#     # while not q.empty():
+#     #     print q.get(),
