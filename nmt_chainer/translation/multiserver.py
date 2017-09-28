ffa159856ffcf30ce08e8c700f6272e567277a94
Frederic Bergeron <bergeron@pa.jst.jp>
2017-09-28 09:30:52 +0900

    Only put sentence translation requests in queue.
    PriorityQueue becomes no longer necessary and has been replaced by a
    simple Queue.

--- nmt_chainer/translation/multiserver.py
+++ nmt_chainer/translation/multiserver.py
@@ -30,10 +30,7 @@ logging.basicConfig()
 log = logging.getLogger("rnns:server")
 log.setLevel(logging.INFO)
 
-TEXT_REQ_PRIORITY = 100
-SENTENCE_REQ_PRIORITY = 2000
-
-class RequestQueue(Queue.PriorityQueue):
+class RequestQueue(Queue.Queue):
 
     def __init__(self):
         Queue.Queue.__init__(self)
@@ -44,38 +41,28 @@ class RequestQueue(Queue.PriorityQueue):
         temp = {}
         log.debug('begin')
         while self._qsize() > 0:
-            priority, item = self._get()
-            #log.debug("priority={0} item={1}".format(priority, item))
+            item = self._get()
+            #log.debug("item={0}".format(item))
             #log.debug("{0} {1}".format(item['session_id'], item['msg_id']))
             #log.debug("{0} {1}".format(item['session_id'], item['article_id']))
             key = "{0}-{1}".format(item['session_id'], item['client_tab_id'])
-            if 'sentence_number' in item:
-                log.debug("{0} {1} {2}".format(priority, key, item['sentence_number']))
-            elif 'type' in item:
-                log.debug("{0} {1} {2}".format(priority, key, item['type']))
+            log.debug("{0} {1}".format(key, item['sentence_number']))
 
             if not key in temp:
                 temp[key] = deque()
-            temp[key].append((priority, item))
+            temp[key].append(item)
         log.debug('end')
         keys = temp.keys()[:]
         log.debug('keys={0}'.format(keys))
         
         log.debug('begin2')
-        r = 0
         while keys:
             for k in keys:
-                priority, item = temp[k].popleft()
+                item = temp[k].popleft()
                 key = "{0}-{1}".format(item['session_id'], item['client_tab_id'])
-                if 'sentence_number' in item:
-                    new_priority = SENTENCE_REQ_PRIORITY + r
-                    log.debug("{0} {1} {2}".format(new_priority, key, item['sentence_number']))
-                elif 'type' in item:
-                    new_priority = TEXT_REQ_PRIORITY + r
-                    log.debug("{0} {1} {2}".format(new_priority, key, item['type']))
-                r += 1
-                #log.debug("priority={0} item={1}".format(new_priority, item))
-                self._put((new_priority, item))
+                log.debug("{0} {1}".format(key, item['sentence_number']))
+                #log.debug("item={0}".format(item))
+                self._put(item)
             keys = [k for k in keys if temp[k]]
         log.debug('end2')
         self.mutex.release()
@@ -88,19 +75,19 @@ class RequestQueue(Queue.PriorityQueue):
         self.mutex.acquire()
         temp = deque()
         while self._qsize() > 0:
-            priority, item = self._get()
+            item = self._get()
             item_key = "{0}-{1}".format(item['session_id'], item['client_tab_id'])
             if 'sentence_number' in item:
-                log.info("{0} {1} {2}".format(priority, item_key, item['sentence_number']))
+                log.info("{0} {1}".format(item_key, item['sentence_number']))
             elif 'type' in item:
-                log.info("{0} {1} {2}".format(priority, item_key, item['type']))
+                log.info("{0} {1}".format(item_key, item['type']))
 
             if item_key != client_id:
-                temp.append((priority, item))
+                temp.append(item)
         log.info('begin2')
         while len(temp) > 0:
-            priority, item = temp.pop()
-            self._put((priority, item))
+            item = temp.pop()
+            self._put(item)
         log.info('end2')
         self.mutex.release()
         log.info("cancel_requests_from {0} end".format(client_id)) 
@@ -114,8 +101,7 @@ class RequestQueue(Queue.PriorityQueue):
 
     def refresh_keepalive_timer(self, client_id, timestamp):
         self.mutex.acquire()
-        for item in self.queue:
-            priority, trans_req = item
+        for trans_req in self.queue:
             key = "{0}-{1}".format(trans_req['session_id'], trans_req['client_tab_id']) 
             if key == client_id:
                 trans_req['keepalive'] = timestamp
@@ -124,7 +110,7 @@ class RequestQueue(Queue.PriorityQueue):
     def remove_expired_requests(self):
         now = time.time()
         self.mutex.acquire()
-        self.queue = [x for x in self.queue if now - x[1]['keepalive'] < 10]
+        self.queue = deque([x for x in self.queue if now - x['keepalive'] < 10])
         self.mutex.release()
 
 class QueueCleaner(threading.Thread):
@@ -154,73 +140,33 @@ class Worker(threading.Thread):
     def run(self):
         lang_pair = "{0}-{1}".format(self.src_lang, self.tgt_lang)
         while True:
-            request_priority, translation_request = self.manager.translation_request_queues[lang_pair].get(True)
+            translation_request = self.manager.translation_request_queues[lang_pair].get(True)
             log.info("request={0}".format(translation_request))
             key = "{0}-{1}".format(translation_request['session_id'], translation_request['client_tab_id']) 
             start_request = timeit.default_timer()
             log.info(timestamped_msg("Thread {0}: Handling request: {1}:{2}".format(self.name, key, translation_request['article_id'])))
 
-            if 'text' in translation_request:
-                log.info("TEXT: {0} {1}".format(datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S') ,translation_request['text'].encode('utf-8')))
-                data = {
-                    'text': translation_request['text'],
-                    'lang_source': translation_request['src_lang'],
-                    'lang_target': translation_request['tgt_lang']
-                }
-                splitter_url = 'http://lotus.kuee.kyoto-u.ac.jp/~frederic/webmt-rnnsearch-multiple-requests/cgi-bin/split_sentences.cgi'
-                r = requests.post(splitter_url, data)
-                if r.status_code == 200:
-                    self.manager.client_cancellations[key] = False 
-
-                    json_resp = r.json()
-
-                    sentence_count = len(json_resp['sentences'])
-                    if key in self.manager.client_responses:
-                        response_queue = self.manager.client_responses[key]
-                    else:
-                        response_queue = Queue.Queue()
-                        self.manager.client_responses[key] = response_queue
-                    response_queue.put(json_resp)
-
-                    log.info("sentenceCount={0}".format(len(json_resp['sentences'])))
-                    for index, sentence in enumerate(json_resp['sentences']):
-                        translate_sentence_request = dict(translation_request)
-                        del translate_sentence_request['text']
-                        translate_sentence_request['sentence'] = sentence
-                        translate_sentence_request['sentence_number'] = index
-                        translate_sentence_request['lang_source'] = self.src_lang
-                        translate_sentence_request['lang_target'] = self.tgt_lang
-                        translate_sentence_request['keepalive'] = time.time()
-                        log.info("new_req={0}".format(translate_sentence_request))
-                        self.manager.translation_request_queues[lang_pair].put((SENTENCE_REQ_PRIORITY, translate_sentence_request))
-                    log.info("q before sz={0}".format(self.manager.translation_request_queues[lang_pair].qsize()))
-                    self.manager.translation_request_queues[lang_pair].redistribute_requests()
-                    log.info("q after sz={0}".format(self.manager.translation_request_queues[lang_pair].qsize()))
-            elif 'sentence' in translation_request:
-                log.info("SENTENCE for worker {0}:{1}: {2}".format(self.host, self.port, translation_request['sentence'].encode('utf-8')))
-                self.manager.add_active_translation(lang_pair, translation_request)
-                client = Client(self.host, self.port)
-                resp = client.query(translation_request['sentence'].encode('utf-8'), 
-                    article_id=translation_request['article_id'],
-                    beam_width=translation_request['beam_width'],
-                    nb_steps=translation_request['nb_steps'],
-                    nb_steps_ratio=translation_request['nb_steps_ratio'],
-                    prob_space_combination=translation_request['prob_space_combination'],
-                    normalize_unicode_unk=translation_request['normalize_unicode_unk'],
-                    remove_unk=translation_request['remove_unk'],
-                    attempt_to_relocate_unk_source=translation_request['attempt_to_relocate_unk_source'],
-                    sentence_id=translation_request['sentence_number'],
-                    attn_graph_width=translation_request['attn_graph_width'],
-                    attn_graph_height=translation_request['attn_graph_height'])
-                self.manager.remove_active_translation(lang_pair, translation_request)
-                json_resp = json.loads(resp)
-                if 'out' in json_resp:
-                    log.info("TRANSLATION: {0}".format(json_resp['out'].encode('utf-8')))
-                    if not key in self.manager.client_cancellations or not self.manager.client_cancellations[key]:
-                        response_queue = self.manager.client_responses[key]
-                        response_queue.put(json_resp)
-                else:
-                    log.info("RESPONSE: {0}".format(json_resp))
+            log.info("SENTENCE for worker {0}:{1}: {2}".format(self.host, self.port, translation_request['sentence'].encode('utf-8')))
+            self.manager.add_active_translation(lang_pair, translation_request)
+            client = Client(self.host, self.port)
+            resp = client.query(translation_request['sentence'].encode('utf-8'), 
+                article_id=translation_request['article_id'],
+                beam_width=translation_request['beam_width'],
+                nb_steps=translation_request['nb_steps'],
+                nb_steps_ratio=translation_request['nb_steps_ratio'],
+                prob_space_combination=translation_request['prob_space_combination'],
+                normalize_unicode_unk=translation_request['normalize_unicode_unk'],
+                remove_unk=translation_request['remove_unk'],
+                attempt_to_relocate_unk_source=translation_request['attempt_to_relocate_unk_source'],
+                sentence_id=translation_request['sentence_number'],
+                attn_graph_width=translation_request['attn_graph_width'],
+                attn_graph_height=translation_request['attn_graph_height'])
+            self.manager.remove_active_translation(lang_pair, translation_request)
+            json_resp = json.loads(resp)
+            log.info("TRANSLATION: {0}".format(json_resp['out'].encode('utf-8')))
+            if not key in self.manager.client_cancellations or not self.manager.client_cancellations[key]:
+                response_queue = self.manager.client_responses[key]
+                response_queue.put(json_resp)
 
             log.info("Request processed in {0} s. by {1} [{2}:{3}]".format(timeit.default_timer() - start_request, self.name, key, translation_request['article_id']))
 
@@ -264,12 +210,8 @@ class Manager(object):
         all_requests = self.translation_request_queues[lang_pair].content() + self.active_translations[lang_pair]
         log.info("all_req={0}".format(all_requests))
         for item in all_requests:
-            if len(item) == 2:
-                priority, actual_item = item
-            else:
-                actual_item = item
-            if 'session_id' in actual_item and 'client_tab_id' in actual_item:
-                client_key = "{0}-{1}".format(actual_item['session_id'], actual_item['client_tab_id'])
+            if 'session_id' in item and 'client_tab_id' in item:
+                client_key = "{0}-{1}".format(item['session_id'], item['client_tab_id'])
                 clients.add(client_key)
         return {'clients': len(clients), 'requests': len(all_requests)};
 
@@ -313,17 +255,48 @@ class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                 log.info(timestamped_msg("Handling request..."))
                 str_data = self.request.recv(16384)
                 log.info("data={0}".format(str_data))
-                json_data = json.loads(str_data)
-                
                 response = {}
+                json_data = json.loads(str_data)
                 lang_pair = "{0}-{1}".format(json_data['src_lang'], json_data['tgt_lang'])
                 key = "{0}-{1}".format(json_data['session_id'], json_data['client_tab_id']) 
                 if json_data['type'] == 'translate':
                     log.info("TRANSLATE from {0}: {1}".format(key, datetime.datetime.fromtimestamp(time.time()).strftime('%Y-%m-%d %H:%M:%S')))
-                    json_data['keepalive'] = time.time()
-                    self.manager.translation_request_queues[lang_pair].put((TEXT_REQ_PRIORITY, json_data))
-                    # self.manager.translation_request_queues[lang_pair].redistribute_requests()
-                    response = {'msg': 'Translation request has been added to queue.'}
+                    data = {
+                        'text': json_data['text'],
+                        'lang_source': json_data['src_lang'],
+                        'lang_target': json_data['tgt_lang']
+                    }
+                    splitter_url = 'http://lotus.kuee.kyoto-u.ac.jp/~frederic/webmt-rnnsearch-multiple-requests/cgi-bin/split_sentences.cgi'
+                    r = requests.post(splitter_url, data)
+                    log.info('Splitter Status: {0}'.format(r.status_code))
+                    if r.status_code == 200:
+                        self.manager.client_cancellations[key] = False 
+
+                        json_resp = r.json()
+
+                        sentence_count = len(json_resp['sentences'])
+                        if key in self.manager.client_responses:
+                            response_queue = self.manager.client_responses[key]
+                        else:
+                            response_queue = Queue.Queue()
+                            self.manager.client_responses[key] = response_queue
+                        response_queue.put(json_resp)
+
+                        log.info("sentenceCount={0}".format(len(json_resp['sentences'])))
+                        for index, sentence in enumerate(json_resp['sentences']):
+                            translate_sentence_request = dict(json_data)
+                            del translate_sentence_request['text']
+                            translate_sentence_request['sentence'] = sentence
+                            translate_sentence_request['sentence_number'] = index
+                            translate_sentence_request['lang_source'] = translate_sentence_request['src_lang']
+                            translate_sentence_request['lang_target'] = translate_sentence_request['tgt_lang']
+                            translate_sentence_request['keepalive'] = time.time()
+                            log.info("new_req={0}".format(translate_sentence_request))
+                            self.manager.translation_request_queues[lang_pair].put(translate_sentence_request)
+                        log.info("q before sz={0}".format(self.manager.translation_request_queues[lang_pair].qsize()))
+                        self.manager.translation_request_queues[lang_pair].redistribute_requests()
+                        log.info("q after sz={0}".format(self.manager.translation_request_queues[lang_pair].qsize()))
+                        response = {'msg': 'All sentences have been queued.'}
                 elif json_data['type'] == 'poll':
                     log.info("POLL from {0}".format(key))   
                     response = self.manager.poll(lang_pair, key)
@@ -346,7 +319,6 @@ class Server(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
                 else:
                     response = {'msg': 'Unknown request type. Request has been ignored.'}
 
-                log.info("q={0} sz={1}".format(self.manager.translation_request_queues[lang_pair], self.manager.translation_request_queues[lang_pair].qsize()))
                 log.info("Request processed in {0} s. by {1}".format(timeit.default_timer() - start_request, threading.current_thread().name))
                 response = json.dumps(response)
                 self.request.sendall(response)
