cee0b63582aedeabedf9339095a68b1c60379e2c
Frederic Bergeron <bergeron@pa.jst.jp>
2017-10-04 15:07:08 +0900

    Added other parameters.

--- nmt_chainer/translation/client.py
+++ nmt_chainer/translation/client.py
@@ -18,23 +18,51 @@ class Client:
         self.ip = server_ip
         self.port = server_port
 
-    def query(self, sentence, article_id=1, beam_width=30, nb_steps=50, nb_steps_ratio=1.5,
-              prob_space_combination=False, normalize_unicode_unk=True, remove_unk=False, attempt_to_relocate_unk_source=False,
-              sentence_id=1, attn_graph_width=0, attn_graph_height=0):
+    def query(self, 
+              sentence, 
+              article_id=1, 
+              beam_width=30, 
+              nb_steps=50, 
+              nb_steps_ratio=1.5,
+              beam_pruning_margin='none',  
+              beam_score_length_normalization='none',
+              beam_score_length_normalization_strength=0.2,
+              post_score_length_normalization='simple',
+              post_score_length_normalization_strength=0.2,
+              beam_score_coverage_penalty='none',
+              beam_score_coverage_penalty_strength=0.2,
+              post_score_coverage_penalty='none',
+              post_score_coverage_penalty_strength=0.2,
+              prob_space_combination=False, 
+              normalize_unicode_unk=True, 
+              remove_unk=False, 
+              attempt_to_relocate_unk_source=False,
+              sentence_id=1, 
+              attn_graph_width=0, 
+              attn_graph_height=0):
 
         query = """<?xml version="1.0" encoding="utf-8"?>
 <article id="{0}"
     beam_width="{1}"
     nb_steps="{2}"
     nb_steps_ratio="{3}"
-    prob_space_combination="{4}"
-    normalize_unicode_unk="{5}"
-    remove_unk="{6}"
-    attempt_to_relocate_unk_source="{7}"
-    attn_graph_width="{8}"
-    attn_graph_height="{9}">
-    <sentence id="{10}">
-        <i_sentence>{11}</i_sentence>
+    beam_pruning_margin="{4}"
+    beam_score_length_normalization="{5}"
+    beam_score_length_normalization_strength="{6}"
+    post_score_length_normalization="{7}"
+    post_score_length_normalization_strength="{8}"
+    beam_score_coverage_penalty="{9}"
+    beam_score_coverage_penalty_strength="{10}"
+    post_score_coverage_penalty="{11}"
+    post_score_coverage_penalty_strength="{12}"
+    prob_space_combination="{13}"
+    normalize_unicode_unk="{14}"
+    remove_unk="{15}"
+    attempt_to_relocate_unk_source="{16}"
+    attn_graph_width="{17}"
+    attn_graph_height="{18}">
+    <sentence id="{19}">
+        <i_sentence>{20}</i_sentence>
     </sentence>
 </article>"""
 
@@ -42,6 +70,15 @@ class Client:
                              beam_width, 
                              nb_steps, 
                              nb_steps_ratio, 
+                             beam_pruning_margin,
+                             beam_score_length_normalization,
+                             beam_score_length_normalization_strength,
+                             post_score_length_normalization,
+                             post_score_length_normalization_strength,
+                             beam_score_coverage_penalty,
+                             beam_score_coverage_penalty_strength,
+                             post_score_coverage_penalty,
+                             post_score_coverage_penalty_strength,                    
                              str(prob_space_combination).lower(),
                              str(normalize_unicode_unk).lower(), 
                              str(remove_unk).lower(), 
