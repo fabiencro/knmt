5429f193c73be4182faa7202e2babb771131f7ea
Frederic Bergeron <bergeron@pa.jst.jp>
2019-02-14 11:29:59 +0900

    Put back server test and fixed it.

--- nmt_chainer/translation/server.py
+++ nmt_chainer/translation/server.py
@@ -135,8 +135,8 @@ class RequestHandler(six.moves.socketserver.BaseRequestHandler):
     def handle(self):
         start_request = timeit.default_timer()
         log.info(timestamped_msg("Handling request..."))
-        data = self.request.recv(4096)
-        text_uid = hashlib.sha1("{0}_{1}".format(start_request, data)).hexdigest()
+        data = self.request.recv(4096).decode('utf-8')
+        text_uid = hashlib.sha1("{0}_{1}".format(start_request, data).encode('utf-8')).hexdigest()
         kw_filename = '/tmp/{0}.kw'.format(text_uid)
 
         response = {}
